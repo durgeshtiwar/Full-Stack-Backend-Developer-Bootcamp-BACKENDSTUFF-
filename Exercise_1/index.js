ce52420f75fdba320cf74f325fb4da25b9bf9a32
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-07-16 20:45:36 +0530

    Start Designing Backend and Auth part-1

--- Exercise_1/index.js
+++ Exercise_1/index.js
@@ -1,12 +1,7 @@
-const express = require("express");
-const app = express();
-const PORT = 3000;
-
-app.get('/',(req,res)=>
-{
-  res.status(200).send("<h1>Hello this is the first program of Backend</h1>");
-})
+const app = require("./app");
+const {PORT} = process.env; 
 app.listen(PORT,()=>
 {
-  console.log("Server is runing");
-})
+  "Server are running 4000";
+}
+)
