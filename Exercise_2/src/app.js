81dec36695c58fb028d211dab12b1d025e211b5a
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-08-08 00:55:54 +0530

    Pending

--- Exercise_2/src/app.js
+++ Exercise_2/src/app.js
@@ -7,15 +7,16 @@ app.use(cors({
   origin : process.env.CORS_ORIGIN,
   Credential : true
 }))
-app.use(express.json({limit : "1mb"}))
-app.use(express.urlencoded({extended:true , limit : "1mb"}))
+app.use(express.json({limit : "16kb"}))
+app.use(express.urlencoded({extended:true , limit : "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser)
 
 
 
 //import routes
-import userRouter from "./routes/user.routes.js"
+//import userRouter from "./routes/user.routes.js"
 //use routes
-app.use("/api/v1/users",userRouter);
+//app.use("/api/v1/users",userRouter);
+
 export {app}
--- Exercise_2/src/controllers/user.controller.js
+++ Exercise_2/src/controllers/user.controller.js
@@ -1,11 +1,8 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
-
-const registerUser = asyncHandler( async (req, res) =>
-  {
-      res.status(200).json({
-      message : "ok"
-    })
-  })
-
-  console.log(registerUser());
-export {registerUser}
+// import { asyncHandler } from "../utils/asyncHandler.js";
+// const registerUser = asyncHandler( async (req, res) =>
+//   {
+//     fetch('https://dummyjson.com/posts')
+//     .then(res => res.json())
+//   })
+//   console.log(registerUser());
+// export {registerUser}
