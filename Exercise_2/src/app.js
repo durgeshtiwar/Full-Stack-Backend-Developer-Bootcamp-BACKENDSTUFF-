3dab07612ed77d91d9ee3a40e2f03b8d63027fb6
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-08-07 12:54:20 +0530

    complete guide for router

--- Exercise_2/src/app.js
+++ Exercise_2/src/app.js
@@ -1,8 +1,8 @@
 import express from "express"
 import cookieParser from "cookie-parser"
-import cors from cors
-const app = express();
 
+const app = express();
+import cors from "cors"
 app.use(cors({
   origin : process.env.CORS_ORIGIN,
   Credential : true
@@ -11,5 +11,13 @@ app.use(express.json({limit : "16kb"}))
 app.use(express.urlencoded({extended:true , limit : "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser)
+
+
+
+//import routes
+import userRouter from "./routes/user.routes.js"
+
+//use routes
+app.use("/api/v1/users",userRouter);
 
 export {app}
