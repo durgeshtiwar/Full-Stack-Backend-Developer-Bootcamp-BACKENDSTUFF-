febed13e18e0d874269068a362edb41509234846
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-08-09 20:17:11 +0530

    Register Data

--- Exercise_2/src/controllers/user.controller.js
+++ Exercise_2/src/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
 import { User } from "../models/user.model.js";
+import { uploadCloudinary } from "../utils/cloudinary.js";
+import { apiResponse } from "../utils/apiResponse.js";
 
 const registerUser = asyncHandler( async (req, res) =>
   {
@@ -29,9 +31,38 @@ const registerUser = asyncHandler( async (req, res) =>
     }
 
 
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
+    if (!avatarLocalPath) {
+      throw new ApiError(400, "avatar fields are required");
+    }
 
+    const avatar = await uploadCloudinary(avatarLocalPath);
+    const coverImage = await uploadCloudinary(coverImageLocalPath);
+    if (!avatar) {
+      throw new ApiError(400, "avatar fields are required");
+    }
 
+    const user = await User.create({
+      fullName,
+      avatar : avatar.url,
+      coverImage : coverImage?.url || "",
+      email,
+      password,
+      username : username.toLowerCase()
+    })
+
+    const createdUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+    )
+    if (!createdUser) {
+      throw new ApiError(500, "Somthing went wrong while registering the user");
+    }
+
+    return res.status(201).json(
+      new apiResponse(200, createdUser, "User Registered Successfully")
+    )
 
 
 
