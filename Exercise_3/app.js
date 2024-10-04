f35f2bd36d750927684575cb43887a4fd8af885c
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-10-04 17:22:51 +0530

    Request and Response

--- Exercise_3/app.js
+++ Exercise_3/app.js
@@ -1,7 +1,44 @@
 const http = require("http");
-const server = http.createServer((res, req)=>{
-  // console.log(res);
-  // console.log(req);
+const server = http.createServer((req, res)=>{
+  console.log(req.url, req.method);
+  if (req.url === '/home') {
+    res.write("<h1>Welcome to Home</h1>")
+    return res.end();
+  }else if (req.url === '/men') {
+    res.write("<h1>Welcome to Men</h1>")
+    return res.end();
+  }else if (req.url === '/women') {
+    res.write("<h1>Welcome to Women</h1>")
+    return res.end();
+  }else if (req.url === '/kid') {
+    res.write("<h1>Welcome to Kid</h1>")
+    return res.end();
+  }else if(req.url === '/cart'){
+    res.write("<h1>Welcome to Cart</h1>")
+    return res.end();
+  }
+
+
+  res.write(`
+    <html lang="en">
+      <head>
+        <title>Document</title>
+      </head>
+      <body>
+        <head>
+          <nav>
+            <ul>
+              <li><a href="/home">Home</a></li>
+              <li><a href="/men">Men</a></li>
+              <li><a href="/women">Women</a></li>
+              <li><a href="/kid">Kid</a></li>
+              <li><a href="/cart">Cart</a></li>
+            </ul>
+          </nav>
+        </head>
+      </body>
+      </html>
+    `);
 })
 const PORT = 3000;
 server.listen(PORT,()=>{
