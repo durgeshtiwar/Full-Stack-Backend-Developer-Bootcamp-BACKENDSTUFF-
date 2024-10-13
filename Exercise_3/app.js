d62feb4d5a4a3288869ddb3f17a151c5a5ec94d1
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-10-13 20:10:39 +0530

    SetUp

--- Exercise_3/app.js
+++
@@ -1,7 +0,0 @@
-const http = require("http");
-const userRequestHandler = require("./user");
-const server = http.createServer(userRequestHandler);
-const PORT = 3000;
-server.listen(PORT,()=>{
-  console.log("server are running at port "+PORT);
-});
